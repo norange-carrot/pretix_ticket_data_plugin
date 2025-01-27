3307a9f39909a3c2b75f9c85ec8f6e195facac77
norange carrot <n.kuechler@posteo.de>
2025-01-27 20:40:43 +0100

    fix linter errors

--- pretix_exporter/exporter.py
+++ pretix_exporter/exporter.py
@@ -1,10 +1,10 @@
-from pretix.base.exporter import ListExporter
-from pretix.base.models.items import Item
 from django_scopes import scope
-from pretix.base.models.orders import Order
 import logging
+from pretix.base.exporter import ListExporter
+from pretix.base.models.orders import Order
 
 logger = logging.getLogger(__name__)
+
 
 class HelperListExporter(ListExporter):
     identifier = "helperlistexporter"
@@ -35,8 +35,12 @@ class HelperListExporter(ListExporter):
                         continue
                     logger.info(f"Processing order {order_position.attendee_email}")
                     d = {
-                        "Vorname": order_position.attendee_name_parts.get('given_name', ''),
-                        "Nachname": order_position.attendee_name_parts.get('family_name', ''),
+                        "Vorname": order_position.attendee_name_parts.get(
+                            "given_name", ""
+                        ),
+                        "Nachname": order_position.attendee_name_parts.get(
+                            "family_name", ""
+                        ),
                         "E-Mail": order.email,
                         "Telefon": order.phone,
                     }
