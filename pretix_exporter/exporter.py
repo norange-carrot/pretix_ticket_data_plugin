496ddec203b2ed403355923d8d0d1877c9486e0e
norange carrot <n.kuechler@posteo.de>
2025-01-29 18:24:39 +0100

    refactor(): decrease function scope

--- pretix_exporter/exporter.py
+++ pretix_exporter/exporter.py
@@ -1,9 +1,11 @@
 import logging
 from django_scopes import scope
 from pretix.base.exporter import ListExporter
-from pretix.base.models.orders import Order
+from pretix.base.models.orders import Order, OrderPosition
 
 logger = logging.getLogger(__name__)
+
+CATEGORY_ID = 2
 
 
 class HelperListExporter(ListExporter):
@@ -11,39 +13,40 @@ class HelperListExporter(ListExporter):
     verbose_name = "Helper Orders as Excel"
 
     def iterate_list(self, form_data):
-        headers = [
+        headers = self._get_headers()
+        yield headers
+
+        with scope(event=self.event):
+            orders = self._get_orders()
+            logger.info(f"Found {len(orders)} orders")
+            for order in orders:
+                self._process_order(order)
+
+    def _get_headers(self) -> list[str]:
+        return [
             "Vorname",
             "Nachname",
             "E-Mail",
             "Telefon",
         ]
-        yield headers
 
-        item_category_id = 2
+    def _get_orders(self) -> list[Order]:
+        return (
+            Order.objects.exclude(status=Order.STATUS_CANCELED)
+            .filter(event=self.event)
+            .all()
+        )
 
-        with scope(event=self.event):
-            orders = (
-                Order.objects.exclude(status=Order.STATUS_CANCELED)
-                .filter(event=self.event)
-                .all()
-            )
-            logger.info(f"Found {len(orders)} orders")
-            for order in orders:
-                logger.info(f"Processing order {order.code}")
-                for order_position in order.positions.all():
-                    logger.info(f"Processing order {order_position.item.category.id}")
-                    if order_position.item.category.id != item_category_id:
-                        continue
-                    logger.info(f"Processing order {order_position.attendee_email}")
-                    d = {
-                        "Vorname": order_position.attendee_name_parts.get(
-                            "given_name", ""
-                        ),
-                        "Nachname": order_position.attendee_name_parts.get(
-                            "family_name", ""
-                        ),
-                        "E-Mail": order.email,
-                        "Telefon": order.phone,
-                    }
+    def _process_order(self, order: Order):
+        logger.info(f"Processing order {order.code}")
+        for order_position in order.positions.all():
+            yield self._extract_data(order, order_position)
 
-                    yield list(d.values())
+    def _extract_data(self, order: Order, order_position: OrderPosition) -> list[str]:
+        if order_position.item.category.id == CATEGORY_ID:
+            return [
+                order_position.attendee_name_parts.get("given_name", ""),
+                order_position.attendee_name_parts.get("family_name", ""),
+                order.email,
+                order.phone,
+            ]
