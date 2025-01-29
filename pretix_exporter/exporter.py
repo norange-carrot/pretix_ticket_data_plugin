32b5d4ddacc3e457a1673e1601332c452ac2637e
norange carrot <n.kuechler@posteo.de>
2025-01-29 18:43:14 +0100

    fix(): make it work again

--- pretix_exporter/exporter.py
+++ pretix_exporter/exporter.py
@@ -20,8 +20,12 @@ class HelperListExporter(ListExporter):
             orders = self._get_orders()
             logger.info(f"Found {len(orders)} orders")
             for order in orders:
-                self._process_order(order)
-
+                logger.info(f"Processing order {order.code}")
+                for order_position in order.positions.all():
+                    output_data = self._process_order(order, order_position)
+                    if output_data:
+                        yield output_data
+                    
     def _get_headers(self) -> list[str]:
         return [
             "Vorname",
@@ -37,12 +41,8 @@ class HelperListExporter(ListExporter):
             .all()
         )
 
-    def _process_order(self, order: Order):
-        logger.info(f"Processing order {order.code}")
-        for order_position in order.positions.all():
-            yield self._extract_data(order, order_position)
-
-    def _extract_data(self, order: Order, order_position: OrderPosition) -> list[str]:
+    def _process_order(self, order: Order, order_position: OrderPosition)-> list[str]:
+        logger.info(f"Processing order position {order_position.id}")
         if order_position.item.category.id == CATEGORY_ID:
             return [
                 order_position.attendee_name_parts.get("given_name", ""),
